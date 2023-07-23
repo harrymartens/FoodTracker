3f3ca0f8ebff161ea028d33bcb6cc184270dec90
Harry Martens <z5394776@ad.unsw.edu.au>
2023-07-23 20:33:05 +1000

    Server Running

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import express, { json, Request, Response } from 'express';
+import morgan from 'morgan';
+import config from './config.json';
+import cors from 'cors';
+import errorHandler from 'middleware-http-errors';
+import YAML from 'yaml';
+import sui from 'swagger-ui-express';
+import fs from 'fs';
+
+
+// Set up web app
+const app = express();
+// Use middleware that allows us to access the JSON body of requests
+app.use(json());
+// Use middleware that allows for access from other domains
+app.use(cors());
+// for producing the docs that define the API
+const file = fs.readFileSync('./swagger.yaml', 'utf8');
+app.get('/', (req: Request, res: Response) => res.redirect('/docs'));
+app.use('/docs', sui.serve, sui.setup(YAML.parse(file), { swaggerOptions: { docExpansion: config.expandDocs ? 'full' : 'list' } }));
+
+const PORT: number = parseInt(process.env.PORT || config.port);
+const HOST: string = process.env.IP || 'localhost';
+
+// for logging errors (print to terminal)
+app.use(morgan('dev'));
+
+
+
+
+
+
+
+
+
+// For handling errors
+app.use(errorHandler());
+
+// start server
+const server = app.listen(PORT, HOST, () => {
+  // DO NOT CHANGE THIS LINE
+  console.log(`⚡️ Server started on port ${PORT} at ${HOST}`);
+});
+
+// For coverage, handle Ctrl+C gracefully
+process.on('SIGINT', () => {
+  server.close(() => console.log('Shutting down server gracefully.'));
+});
